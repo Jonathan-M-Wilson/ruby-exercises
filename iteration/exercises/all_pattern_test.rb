2588353b52b83c96eea79aa76168141092866da0
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-29 08:07:47 -0600

    :heavy_check_mark: Pass all_pattern_test

--- iteration/exercises/all_pattern_test.rb
+++ iteration/exercises/all_pattern_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class AllPatternTest < Minitest::Test
+
   def test_1
     account_balances = [0, 0, 0, 0, 0, 0, 0]
     all_zeros = true
@@ -16,106 +17,106 @@ class AllPatternTest < Minitest::Test
       checking: 0,
       saving: 0,
       retirement_401k: 0,
-      retirement_ira: 0,
+      retirement_ira: 0
     }
     all_zeros = true
-    account_balances.each do |account, balance|
-      if !balance.zero?
-        all_zeros = false
-      end
+    account_balances.each do |_account, balance|
+      all_zeros = false unless balance.zero?
     end
     assert_equal true, all_zeros
   end
 
   def test_3
-    skip
-    words = ["love", "hate", "fire", "bird", "call"]
+    words = %w[love hate fire bird call]
     all_four_letters = true
     words.each do |word|
-      # Your Code Here
+      all_four_letters = false if word.size != 4
     end
     assert_equal true, all_four_letters
   end
 
   def test_4
-    skip
     words = {
-      one: "love",
-      two: "hate",
-      three: "fire",
-      four: "bird",
-      five: "call"
+      one: 'love',
+      two: 'hate',
+      three: 'fire',
+      four: 'bird',
+      five: 'call'
     }
     all_four_letters = true
-    words.each do |position, word|
-      # Your Code Here
+    words.each do |_position, word|
+      all_four_letters = false if word.size != 4
     end
     assert_equal true, all_four_letters
   end
 
   def test_5
-    skip
-    statuses = [:busy, :busy, :busy]
+    statuses = %i[busy busy busy]
     all_busy = true
-    # Your Code Here
-
+    statuses.each do |status|
+      all_busy = false if status != :busy
+    end
     assert_equal true, all_busy
   end
 
   def test_6
-    skip
     friend_status = {
-      "Megan" => :busy,
-      "Sarah" => :busy,
-      "Duncan" => :busy,
+      'Megan' => :busy,
+      'Sarah' => :busy,
+      'Duncan' => :busy
     }
     all_busy = true
-    # Your Code Here
-
+    friend_status.each do |_person, status|
+      all_busy = false if status != :busy
+    end
     assert_equal true, all_busy
   end
 
   def test_7
-    skip
-    zip_codes = [94381, 831, 50009, 36232, 8992, 89999, 11110]
-    # Your Code Here
-
+    zip_codes = [94_381, 831, 50_009, 36_232, 8992, 89_999, 11_110]
+    all_five_digits = true
+    zip_codes.each do |code|
+      all_five_digits = false if code.size != 5
+    end
     assert_equal false, all_five_digits
   end
 
   def test_8
-    skip
     zip_codes = {
-      "Megan" => 94381,
-      "Sarah" => 831,
-      "Duncan" => 50009,
-      "Raymart" => 36232,
-      "Alec" => 89092,
-      "Cameron" => 89999,
-      "Joshua" => 11110
+      'Megan' => 94_381,
+      'Sarah' => 831,
+      'Duncan' => 50_009,
+      'Raymart' => 36_232,
+      'Alec' => 89_092,
+      'Cameron' => 89_999,
+      'Joshua' => 11_110
     }
-    # Your Code Here
-
+    all_five_digits = true
+    zip_codes.each do |_person, code|
+      all_five_digits = false if code.size != 5
+    end
     assert_equal false, all_five_digits
   end
 
   def test_9
-    skip
-    snacks = ["GARLIC PLANTAINS", "SNICKERDOODLES", "Pretzels"]
-    # Your Code Here
-
+    snacks = ['GARLIC PLANTAINS', 'SNICKERDOODLES', 'Pretzels']
+    all_caps = true
+    snacks.each do |snack|
+      all_caps = false if snack.upcase != true
+    end
     assert_equal false, all_caps
   end
 
   def test_10
-    skip
     snacks = {
-      savory: "GARLIC PLANTAINS",
-      sweet: "SNICKERDOODLES",
-      salty: "Pretzels"
+      savory: 'GARLIC PLANTAINS',
+      sweet: 'SNICKERDOODLES',
+      salty: 'Pretzels'
     }
-    # Your Code Here
-
+    all_caps = true
+    snacks.each do |_type, snack|
+      all_caps = false if snack.upcase != true
+    end
     assert_equal false, all_caps
   end
 end
